df66aac42110493e7861223e7df4d06aabe5ada8
Halil_Hasmer <halilpython@gmail.com>
2022-03-31 17:20:12 +0300

    Initial commit

---
+++ day71_scratchpad.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import csv
+
+
+a = [i for i in range(1,11)]
+
+b = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+with open("71-v2/csv_try.csv", "w") as df:
+    writer = csv.writer(df)
+    writer.writerow(["a_list", "b_list"])
+    for i in range(len(a)):
+        writer.writerow([a[i], b[i]])
+
+
+with open("71-v2/csv_try_2.csv", "w") as df:
+    writer = csv.writer(df)
+    writer.writerow(["a_list", "b_list"])
+    writer.writerows([a, b])
